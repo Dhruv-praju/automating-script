e8a18ef1393a20160eb7ff46866ce760be2414a1
Dhruv Prajapati <dhruv16@bu.edu>
2025-05-04 11:07:20 -0400

    e13

--- hello.py
+++ hello.py
@@ -3,5 +3,6 @@ from datetime import datetime
 # writes 'Hello Name' in log.txt file with current timestamp
 import random as rand
 names = ['Dhruv','dhruv','DHRUV', 'Dhruv Prajapati', 'DHRUV']
-with open('./log.txt', 'a') as f:
-    f.write(f"{datetime.now()}: Hello {names[rand.randint(0,3)]}\n")
+# with open('./log.txt', 'a') as f:
+#     f.write(f"{datetime.now()}: Hello {names[rand.randint(0,3)]}\n")
+print(f"{datetime.now()}: Hello {names[rand.randint(0,3)]}")
