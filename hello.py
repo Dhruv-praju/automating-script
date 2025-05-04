bc1912bcdfb4940c8e44a82e1cd3078e0086a0f9
Dhruv Prajapati <dhruv16@bu.edu>
2025-05-03 23:18:47 -0400

    e2

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+# from datetime import datetime
 
 
 # writes 'hello world' in log.txt file with current timestamp
-with open('/tmp/log.txt', 'a') as f:
-    f.write(f"{datetime.now()}: hello world\n")
+# with open('/tmp/log.txt', 'a') as f:
+#     f.write(f"{datetime.now()}: hello world\n")
 
+print("hello world")
