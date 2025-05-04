4be00cf4fb4eaa98abee418d3157c6105a663af1
Dhruv Prajapati <dhruv16@bu.edu>
2025-05-03 22:43:14 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+# writes 'hello world' in log.txt file with current timestamp
+with open('/tmp/log.txt', 'a') as f:
+    f.write(f"{datetime.now()}: hello world\n")
+
